f318907042be21ed4b477a6933423f7c4f1d0b0c
thomas <tg296@.mit.edu>
2016-08-31 10:31:27 -0400

    Updated Taxonomy.py

--- scripts/Taxonomy.py
+++ scripts/Taxonomy.py
@@ -14,7 +14,7 @@ import pandas as pd
 def GGID_lookup(GG_ID, taxonomy_file=0):
     # Takes as input a GreenGenes ID and returns a latin name.  Optionally, can specify which GG taxonomy file to use.  By default uses 97% reference set.
     if(taxonomy_file == 0):
-        taxonomy_file = '/home/ubuntu/databases/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt'
+        taxonomy_file = '/home/ubuntu/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt'
 
     # Extract latin names for OTU IDs
     with open(taxonomy_file, 'r') as fid:
@@ -28,7 +28,7 @@ def GGID_lookup(GG_ID, taxonomy_file=0):
 def convert_GGIDs_to_latin_names(OTU_table, new_OTU_table, taxonomy_file=0):
     # Takes as input a closed-reference OTU table with GreenGenes IDs and returns the same table but with latin names instead of GG IDs.
     if(taxonomy_file == 0):
-        taxonomy_file = '/home/ubuntu/databases/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt'
+        taxonomy_file = '/home/ubuntu/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt'
 
     # Extract OTU IDs
     with open(OTU_table, 'r') as fid:
@@ -55,56 +55,40 @@ def convert_GGIDs_to_latin_names(OTU_table, new_OTU_table, taxonomy_file=0):
         for i in range(1,len(OTU_table_lines)):
             fid.write(OTU_table_lines[i] + '\n')
 
-def convert_GGID_dict_to_latin_names(alignment_dict):
-    # Convert alignment dictionary with GreenGene IDs to latin names
-    new_dict = {}
-    for key in alignment_dict:
-        assignments = alignment_dict[key]
-        latin_names = [GGID_lookup(ass) for ass in assignments if ass != '*']
-        new_dict[key] = latin_names
-    return new_dict
-
-
-def collapse_taxonomic_contents(OTU_table, taxonomic_level, parent_node=0, counts=False):
+def collapse_taxonomic_contents(OTU_table, taxonomic_level, parent_node=0):
     # Takes as input an OTU table with latin names as OTU IDs,
     # the taxonomic level to which all taxonomies should be collapsed.
     # Accepts: kingdom, phylum, class, order, family, genus, species
     # e.g.  collapse_taxonomic_contents(OTU_table, class, Firmicutes)
     # If no parent node is specified, returns all clades of the given taxonomic level.
-    # If counts is left unspecificed, it will return relative abundances by default.  Set counts=True
-    # to collapse counts as well.
 
     with open(OTU_table, 'r') as fid:
         OTU_table_lines = fid.readlines()
         OTU_IDs = [line.split()[0] for line in OTU_table_lines]
         OTU_IDs = OTU_IDs[1:]
 
-    # If OTU table contains 'unidentified' reads, remove those from the list of IDs.
-    # They will be added at the end.
-    OTU_IDs = [OTU_ID for OTU_ID in OTU_IDs if OTU_ID != 'unidentified']
-
     # Collapse to the right level        
     if(taxonomic_level == "domain"):
         OTU_taxa = [OTU_ID.split(';')[0] for OTU_ID in OTU_IDs]
     if(taxonomic_level == "phylum"):
         OTU_taxa = [';'.join(OTU_ID.split(';')[:2]) for OTU_ID in OTU_IDs]
-        OTU_taxa_parents = [OTU_ID.split(';')[0][3:] for OTU_ID in OTU_IDs]
+        OTU_taxa_parents = [OTU_ID.split(';')[0][3:] for OTU_ID in OTU_IDs if len(OTU_ID.split(';')[0]) > 3]
     if(taxonomic_level == "class"):
         OTU_taxa = [';'.join(OTU_ID.split(';')[:3]) for OTU_ID in OTU_IDs]
-        OTU_taxa_parents = [OTU_ID.split(';')[1][3:] for OTU_ID in OTU_IDs]
+        OTU_taxa_parents = [OTU_ID.split(';')[1][3:] for OTU_ID in OTU_IDs if len(OTU_ID.split(';')[1]) > 3]
     if(taxonomic_level == "order"):
         OTU_taxa = [';'.join(OTU_ID.split(';')[:4]) for OTU_ID in OTU_IDs]
-        OTU_taxa_parents = [OTU_ID.split(';')[2][3:] for OTU_ID in OTU_IDs]
+        OTU_taxa_parents = [OTU_ID.split(';')[2][3:] for OTU_ID in OTU_IDs if len(OTU_ID.split(';')[2]) > 3]
     if(taxonomic_level == "family"):
         OTU_taxa = [';'.join(OTU_ID.split(';')[:5]) for OTU_ID in OTU_IDs]
-        OTU_taxa_parents = [OTU_ID.split(';')[3][3:] for OTU_ID in OTU_IDs]        
+        OTU_taxa_parents = [OTU_ID.split(';')[3][3:] for OTU_ID in OTU_IDs if len(OTU_ID.split(';')[3]) > 3]
     if(taxonomic_level == "genus"):
         OTU_taxa = [';'.join(OTU_ID.split(';')[:6]) for OTU_ID in OTU_IDs]
-        OTU_taxa_parents = [OTU_ID.split(';')[4][3:] for OTU_ID in OTU_IDs]
+        OTU_taxa_parents = [OTU_ID.split(';')[4][3:] for OTU_ID in OTU_IDs if len(OTU_ID.split(';')[4]) > 3]
     if(taxonomic_level == "species"):
         OTU_taxa = [';'.join(OTU_ID.split(';')[:7]) for OTU_ID in OTU_IDs]
-        OTU_taxa_parents = [OTU_ID.split(';')[5][3:] for OTU_ID in OTU_IDs]
-
+        OTU_taxa_parents = [OTU_ID.split(';')[5][3:] for OTU_ID in OTU_IDs if len(OTU_ID.split(';')[5]) > 3]
+        
 
     # Get indices of each unique taxon
     taxa_indices = {}
@@ -132,9 +116,8 @@ def collapse_taxonomic_contents(OTU_table, taxonomic_level, parent_node=0, count
         OTU_table_counts[i-1,:] = OTU_table_lines[i].split('\t')[1:]
     
     # Normalize counts by sample (column)
-    if counts == False:
-        for i in range(nSamples):
-            OTU_table_counts[:,i] = np.divide(OTU_table_counts[:,i], np.sum(OTU_table_counts[:,i]))
+    for i in range(nSamples):
+        OTU_table_counts[:,i] = np.divide(OTU_table_counts[:,i], np.sum(OTU_table_counts[:,i]))
     
     # Get sample contents for each taxa of the chosen level
     taxa_dict = {}
@@ -145,15 +128,6 @@ def collapse_taxonomic_contents(OTU_table, taxonomic_level, parent_node=0, count
             taxa_dict[key][sample_labels[i]] = np.sum(OTU_table_counts[indices,i])
 
     return taxa_dict
-
-
-def collapse_OTU_table(OTU_table, output_OTU_table, taxonomic_level, parent_node=0):
-    # Collapse OTU table to given taxonomic level
-    taxon_dict = collapse_taxonomic_contents(OTU_table, taxonomic_level, parent_node, counts=True)
-    OTU_IDs = taxon_dict.keys()
-    x = pd.DataFrame.from_dict(taxon_dict)
-    x.transpose().to_csv(output_OTU_table, sep='\t', index_label='OTU_ID')
-    return x.transpose()
 
 
 def extract_available_taxa(OTU_table):
@@ -232,7 +206,49 @@ def get_taxonomic_level_name(latin_name_string, taxonomic_level):
         return latstr.split(';')[5].split('__')[-1]
     if(taxonomic_level == "species"):
         return latstr.split(';')[6].split('__')[-1]
-    
+
+
+def extract_abundance_of_unassigned_OTUs(OTU_table, taxon_name):
+    # Extracts the abundances of all OTUs that have a given taxonomy, e.g. 'k__Bacteria;p__Firmicutes;c__;o__;f__;g__;s__'.
+
+    # Load list of samples in the order they appear in the OTU table
+    with open(OTU_table, 'r') as fid:
+        OTU_table_lines = fid.readlines()
+        samples = OTU_table_lines[0].split('\t')[1:]
+        samples[-1] = samples[-1].rstrip('\n')
+        OTU_IDs = [line.split()[0] for line in OTU_table_lines]
+        OTU_IDs = OTU_IDs[1:]
+        OTU_taxa = [';'.join(OTU_ID.split(';')[:7]) for OTU_ID in OTU_IDs]
+
+    # Get indices of each OTU corresponding to the desired taxon
+    taxon_indices = []
+    for i in range(len(OTU_taxa)):
+        if OTU_taxa[i] == taxon_name:
+            taxon_indices.append(i)
+
+    if taxon_indices == []:
+        return []
+
+    # Read in OTU abundances
+    nOTUs = len(OTU_IDs)
+    nSamples = len(samples)
+
+    # Load OTU table row major order
+    OTU_table_counts = np.zeros((nOTUs, nSamples))
+    for i in range(1,nOTUs+1):
+        OTU_table_counts[i-1,:] = OTU_table_lines[i].split('\t')[1:]
+
+    # Normalize counts by sample (column)
+    for i in range(nSamples):
+        OTU_table_counts[:,i] = np.divide(OTU_table_counts[:,i], np.sum(OTU_table_counts[:,i]))
+
+    # Get sample contents for each taxa of the chosen level
+    taxon_abundances = []
+    for i in range(nSamples):
+        taxon_abundances.append(np.sum(OTU_table_counts[taxon_indices, i]))
+
+    return taxon_abundances
+
 
 def extract_abundance_of_taxon(OTU_table, taxon_name, taxonomic_level, parent_node=0):
     # Returns a vector of abundances of the given taxon, where indices correspond to samples in the OTU table.  Takes as input the OTU table, the name of the taxon (e.g. 'Clostridium'), the taxonomic level (e.g. 'genus'), and the parent node (e.g. 'Clostridiaceae').
@@ -259,7 +275,6 @@ def extract_abundance_of_taxon(OTU_table, taxon_name, taxonomic_level, parent_no
                 abundance_vector.append(abundances[samples[i]])
             return abundance_vector
         else:
-            print "Taxon " + taxon + " not found!"
             return 0
     else:
         print "Domain level not supported!"
